94368ea027eeb129bb66f27a93e6d27cd9e078a0
HenriqueGSG <henriqueguarnieri.gg@gmail.com>
2023-06-14 12:00:11 -0300

    Text And Logo update   commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -17,7 +17,36 @@ const Sidebar = () => {
   } = useGetListOrgs();
 
   if (listOrgsLoading) {
-    return "loading";
+    return (
+      <div className="fixed z-50 sm:w-14 sm:h-full sm:bg-white">
+        <aside
+          id="default-sidebar"
+          className={`fixed top-0 left-0 z-50 h-screen max-w-60 transition-transform ${
+            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+          aria-label="Sidebar"
+        >
+          <div className="relative h-full px-3 py-16 overflow-y-auto text-black bg-white dark:bg-gray-800">
+            <button
+              onClick={() => setSidebarOpen(false)}
+              className="absolute inline-flex items-center p-2 text-sm text-white rounded-lg bg-gold top-3 hover:bg-blueMiddle left-3 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            >
+              <BsArrowBarLeft size={20} />
+            </button>
+            <Link to="/" className="absolute flex items-center mb-5 bottom-4">
+              <img src={logo} className="w-10 h-10 " alt="Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap font-merriweather">
+                AthenaConnect
+              </span>
+            </Link>
+
+            <ul className="space-y-2 font-medium">
+              <div className="font-merriweather"></div>
+            </ul>
+          </div>
+        </aside>
+      </div>
+    );
   }
 
   if (listOrgsError) {
